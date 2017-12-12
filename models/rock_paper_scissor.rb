09675d22fc0926f868a53b39c5d03b48945c336d
Alison Wood <alisonwood29@gmail.com>
2017-12-12 21:57:32 +0000

    created ruby modeland finally got tests passing

--- models/rock_paper_scissor.rb
+++ models/rock_paper_scissor.rb
@@ -1,20 +1,31 @@
+
 class Game
 
-  # attr_ac :player_1, :player_2
+  attr_accessor :player_1, :player_2
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
   end
 
-  def result
-    return "Rock wins!" if @player_1 == 'rock' && @player_2 == 'scissors'
-    return "Paper wins!" if @player_1 == 'rock' && @player_2 == 'paper'
-    return "Scissors wins!" if @player_1 == 'paper' && @player_2 == 'scissors'
-    return "Paper wins!" if @player_1 == 'paper' && @player_2 == 'rock'
-    return "Rock wins!" if @player_1 == 'scissors' && @player_2 == 'rock'
-    return "Scissors wins!" if @player_1 == 'scissors' && @player_2 == 'paper'
-    return "Try again!" if @player_1 == @player_2
+  def result()
+     if @player_1 == "rock" && @player_2 == "scissors"
+       return "Rock wins!"
+     elsif @player_1 == "rock" && @player_2 == "paper"
+       return "Paper wins!"
+     elsif @player_1 == "paper" && @player_2 == "scissors"
+       return "Scissors wins!"
+     elsif @player_1 == "paper" && @player_2 == "rock"
+       return "Paper wins!"
+     elsif @player_1 == "scissors" && @player_2 == "rock"
+       return "Rock wins!"
+     elsif @player_1 == "scissors" && @player_2 == "paper"
+       return "Scissors wins!"
+     elsif @player_1 == @player_2
+       return "Try again!"
+     else
+       return "Game over"
+     end
   end
 
 
--- specs/rock_paper_scissor_spec.rb
+++ specs/rock_paper_scissor_spec.rb
@@ -1,4 +1,5 @@
 require("minitest/autorun")
+require("minitest/rg")
 require_relative("../models/rock_paper_scissor")
 
 
@@ -9,40 +10,48 @@ class TestGame < MiniTest::Test
   end
 
   def test_game__rock_v_scissors
-    new_game = Game.new(rock, scissors)
+    game_1 = Game.new("rock", "scissors")
+    assert_equal("Rock wins!", game_1.result())
+  end
+
+  def test_game__rock_v_paper
+    new_game = Game.new("rock", "paper")
+    assert_equal("Paper wins!", new_game.result())
+  end
+
+  def test_game__paper_v_scissors
+    new_game = Game.new("paper", "scissors")
+    assert_equal("Scissors wins!", new_game.result())
+  end
+
+  def test_game__paper_v_rock
+    new_game = Game.new("paper", "rock")
+    assert_equal("Paper wins!", new_game.result())
+  end
+
+  def test_game__scissors_v_rock
+    new_game = Game.new("scissors", "rock")
     assert_equal("Rock wins!", new_game.result())
   end
-  def test_game__rock_v_paper
-    new_game = Game.new(rock, paper)
-    assert_equal("Paper wins!", result)
-  end
-  def test_game__paper_v_scissors
-    result = play_game(paper, scissors)
-    assert_equal("Scissors wins!", result)
-  end
-  def test_game__paper_v_rock
-    result = play_game(paper, rock)
-    assert_equal("Paper wins!", result)
-  end
-  def test_game__scissors_v_rock
-    result = play_game(rock, scissors)
-    assert_equal("Rock wins!", result)
-  end
+
   def test_game__scissors_v_paper
-    result = play_game(rock, scissors)
-    assert_equal("Scissors wins!", result)
+    new_game = Game.new("scissors", "paper")
+    assert_equal("Scissors wins!", new_game.result())
   end
+
   def test_game__rock_v_rock
-    result = play_game(rock, scissors)
-    assert_equal("Try again!", result)
+    new_game = Game.new("rock", "rock")
+    assert_equal("Try again!", new_game.result())
   end
+
   def test_game__paper_v_paper
-    result = play_game(rock, scissors)
-    assert_equal("Try again!", result)
+    new_game = Game.new("paper", "paper")
+    assert_equal("Try again!", new_game.result())
   end
+  
   def test_game__scissors_v_scissors
-    result = play_game(rock, scissors)
-    assert_equal("Try again!", result)
+    new_game = Game.new("scissors", "scissors")
+    assert_equal("Try again!", new_game.result())
   end
 
 end
